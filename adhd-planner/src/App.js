fc612fe8e29b580e1467bdb9734d7c452e5225f8
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-09-26 01:13:16 +0300

    beginning

--- adhd-planner/src/App.js
+++ adhd-planner/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import './Plans.css';
+import Plans from './Plans';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='for-adhd'>
+   
+      <h2 className='topic'>for ADHDiers...</h2>
+      </div>
+      <p> "You will probably forget to enter this website.
+It's not a problem. Stop blaming yourself for everything you've forgotten."</p>
+
+<Plans />
+
     </div>
+
   );
 }
 
---
+++ adhd-planner/src/Plans.js
@@ -0,0 +1,17 @@
+import './Plans.css';
+
+function Plans() {
+  return (
+    <div className="Plans">
+
+<div className="daily">my daily plan</div>
+      <div className="weekly">my weekly plan</div>
+      <div className="monthly">my monthly plan</div>
+      <div className="yearly">my yearly plan</div>
+    </div>
+
+
+  );
+}
+
+export default Plans;
