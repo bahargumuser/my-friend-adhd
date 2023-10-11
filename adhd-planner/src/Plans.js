77072d1636d54368410a6b7166f3196b81f3d999
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-10-12 00:42:45 +0300

    Add buttons

--- adhd-planner/src/Plans.js
+++ adhd-planner/src/Plans.js
@@ -1,17 +1,28 @@
+import React from 'react';
 import './Plans.css';
 
 function Plans() {
   return (
+
     <div className="Plans">
-
-<div className="daily">my daily plan</div>
-      <div className="weekly">my weekly plan</div>
-      <div className="monthly">my monthly plan</div>
-      <div className="yearly">my yearly plan</div>
+      <div className="column">
+        <div className="daily">my daily plan</div>
+        <div className="button">Add Daily Plan</div>
+      </div>
+      <div className="column">
+        <div className="weekly">my weekly plan</div>
+        <div className="button">Add Weekly Plan</div>
+      </div>
+      <div className="column">
+        <div className="monthly">my monthly plan</div>
+        <div className="button">Add Monthly Plan</div>
+      </div>
+      <div className="column">
+        <div className="yearly">my yearly plan</div>
+        <div className="button">Add Yearly Plan</div>
+      </div>
     </div>
-
-
+  
   );
 }
-
 export default Plans;
