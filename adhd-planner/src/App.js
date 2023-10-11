b69bcf8ce92cd89516ed9c0de7a99e57bff586dd
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-10-12 00:42:25 +0300

    regulation

--- adhd-planner/src/App.js
+++ adhd-planner/src/App.js
@@ -9,7 +9,7 @@ function App() {
    
       <h2 className='topic'>for ADHDiers...</h2>
       </div>
-      <p> "You will probably forget to enter this website.
+      <p className='topicTitle'> "You will probably forget to enter this website.
 It's not a problem. Stop blaming yourself for everything you've forgotten."</p>
 
 <Plans />
